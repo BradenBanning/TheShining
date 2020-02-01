528cec1fc3b9161c9970ee76d7276783a30cebe2
Braden <Braden.Banning@gmail.com>
2020-02-01 07:50:34 -0800

    SO's Added

---
+++ Assets/Scripts/CarTurn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CarTurn : MonoBehaviour
+{
+    private float _Timer;
+    private float _TimerLimit;
+
+    private void Update()
+    {
+        _Timer += Time.deltaTime;
+
+        if (_Timer >= _TimerLimit)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Scriptables/SOGameObjectRef.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptables/GORef")]
+public class SOGameObjectRef : ScriptableObject
+{
+    public SOGameObjectRef Ref { get; private set; }
+    public GameObject Value { get; private set; }
+    
+    public void CreateRunTimeObject(GameObject val)
+    {
+        Ref = ScriptableObject.CreateInstance<SOGameObjectRef>();
+        Ref.SetValue(val);
+    }
+
+    public void SetValue(GameObject val)
+    {
+        Value = val;
+    }
+}
