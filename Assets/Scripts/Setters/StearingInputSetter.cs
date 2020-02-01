338b3f3e9c372efec335165fbd7b6f5c0df26bcf
Braden <Braden.Banning@gmail.com>
2020-02-01 10:57:28 -0800

    Wheel Inputs Added

---
+++ Assets/Scripts/Setters/StearingInputSetter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StearingInputSetter : MonoBehaviour
+{
+
+    [SerializeField] private SOStearingInputs _StrearingInputSO;
+    private StearingInputs _StearingInput;
+
+    private void Awake()
+    {
+        _StearingInput = GetComponent<StearingInputs>();
+        _StrearingInputSO.CreateRunTimeObject(_StearingInput);
+    }
+}
