41ec9a2034eb70bfc6308272b98db08bb9992fc4
Braden <Braden.Banning@gmail.com>
2020-01-31 22:51:44 -0800

    Main Scene Established

---
+++ Assets/Scripts/RoadLerp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadLerp : MonoBehaviour
+{
+    private void Update()
+    {
+        //_Timer += Time.deltaTime;
+        
+    }
+}
