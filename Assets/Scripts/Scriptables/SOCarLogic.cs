39c164db3816801e17457d2cb97323102c7ee1a2
Braden <Braden.Banning@gmail.com>
2020-02-01 09:00:37 -0800

    CarMovementFrameworkAdded

---
+++ Assets/Scripts/Scriptables/SOCarLogic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptables/CarLogic")]
+public class SOCarLogic : ScriptableObject
+{
+    public SOCarLogic Ref { get; private set; }
+    public CarTurn CarTurnRef { get; private set; }
+    public CarLateralMovement CarMoveRef { get; private set; }
+    
+    public void CreateRunTimeObject(CarTurn val1, CarLateralMovement val2)
+    {
+        Ref = ScriptableObject.CreateInstance<SOCarLogic>();
+        Ref.SetValue(val1 , val2);
+    }
+
+    public void SetValue(CarTurn val1, CarLateralMovement val2)
+    {
+        CarTurnRef = val1;
+        CarMoveRef = val2;
+    }
+
+}
---
+++ Assets/Scripts/Setters/GameObjectSetter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectSetter : MonoBehaviour
+{
+    [SerializeField] private SOGameObjectRef _GameObjectSO;
+    private GameObject _GameObject;
+
+
+    private void Awake()
+    {
+        _GameObject = gameObject;
+        _GameObjectSO.CreateRunTimeObject(_GameObject);
+    }
+}
