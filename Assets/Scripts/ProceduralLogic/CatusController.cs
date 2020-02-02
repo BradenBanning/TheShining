2387dc2f8fd593d6abbb3238d9ccc6fa210a8a23
Braden <Braden.Banning@gmail.com>
2020-02-01 18:43:58 -0800

    Cactus Controller Structure Established

---
+++ Assets/Scripts/ProceduralLogic/CatusController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatusController : MonoBehaviour
+{
+    private IsCactus[] _CactusArray;
+
+    private void Awake()
+    {
+        _CactusArray = GetComponentsInChildren<IsCactus>();
+    }
+}
