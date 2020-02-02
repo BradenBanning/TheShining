c7778334c0d01088eab82656deda0e16ab19cd29
Braden <Braden.Banning@gmail.com>
2020-02-01 17:54:20 -0800

    Setter Added

---
+++ Assets/Scripts/Scriptables/SOSwitchState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptables/SwitchState")]
+public class SOSwitchState : ScriptableObject
+{
+    public SOSwitchState Ref { get; private set; }
+    public SwitchStatesInput Value { get; private set; }
+
+    public void CreateRunTimeObject(SwitchStatesInput val)
+    {
+        Ref = ScriptableObject.CreateInstance<SOSwitchState>();
+        Ref.SetValue(val);
+    }
+
+    public void SetValue(SwitchStatesInput val)
+    {
+        Value = val;
+    }
+}
