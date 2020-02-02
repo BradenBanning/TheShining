096785297f2b08e551c0f42512a26dfd9f0e0167
Braden <Braden.Banning@gmail.com>
2020-02-02 15:16:48 -0800

    Scroll Wheel Movement Added

--- Assets/Scripts/InputLogic/SwitchStatesInput.cs
+++ Assets/Scripts/InputLogic/SwitchStatesInput.cs
@@ -9,12 +9,15 @@ public class SwitchStatesInput : MonoBehaviour
 {
     public Action SwitchStatesEvent = new Action(delegate {  });
 
+    private int _IsInDriveState;
+
 
     private void Update()
     {
-        if (Input.GetButtonDown("Switch"))
+        if (Input.GetButtonDown("Switch") || (Input.mouseScrollDelta.y < 0 && (_IsInDriveState == 0 || _IsInDriveState % 2 == 0)) || (Input.mouseScrollDelta.y > 0 && (_IsInDriveState % 2 != 0)))
         {
             StatesSwitched();
+            _IsInDriveState++;
         }
     }
     
