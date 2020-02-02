5ea7662a1c3b9737ecbc9f627b7203b2ac7d501e
Braden <Braden.Banning@gmail.com>
2020-02-02 09:23:55 -0800

    Road Lerp Given Limit && GameTimer && GameWin Added

---
+++ Assets/Scripts/GameTimeLogic/GameTimer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTimer : MonoBehaviour
+{
+    [SerializeField] private SOTimer _GameTimerRef;
+
+    [SerializeField] private SOGameWinEvent _GameWinEventRef;
+
+    [SerializeField] private float _GamePhaseTimeLimit = 480f;
+
+    [SerializeField] private int _NumberOfPhasesPerGame = 60;
+
+    public int CurrentPhase { get; private set; } = 0;
+
+    private void Awake()
+    {
+        _GameTimerRef.SetTimeLimit(_GamePhaseTimeLimit);
+        _GameTimerRef.TryStartTimer();
+    }
+
+    private void OnEnable()
+    {
+        _GameTimerRef.IsTimerRunning += TimerReachedLimit;
+    }
+    
+    private void OnDisable()
+    {
+        _GameTimerRef.IsTimerRunning -= TimerReachedLimit;
+    }
+
+    private void TimerReachedLimit(bool val)
+    {
+        _GameTimerRef.TryStartTimer();
+        CurrentPhase++;
+
+        if (CurrentPhase > _NumberOfPhasesPerGame)
+        {
+            _GameWinEventRef.GameWinHappened.Invoke();            
+        }
+        
+
+    }
+}
