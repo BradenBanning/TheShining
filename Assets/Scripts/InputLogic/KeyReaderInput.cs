7ad848bce8c446a37828158660dc4a8ab22db194
Braden <Braden.Banning@gmail.com>
2020-02-01 22:24:44 -0800

    Input Reader Added

---
+++ Assets/Scripts/InputLogic/KeyReaderInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyReaderInput : MonoBehaviour
+{
+    private void Update()
+    {
+        foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(vKey))
+            {
+                //your code here
+            }
+        }
+    }
+}
