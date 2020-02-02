28a2479b3b0496c5e23b87288f01e68055bdada7
Braden <Braden.Banning@gmail.com>
2020-02-01 22:10:46 -0800

    Text analyzer Added

---
+++ Assets/Scripts/Scriptables/SOTextBoxBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SOTextBoxBehaviour : MonoBehaviour
+{
+    
+    [SerializeField] private String[] _GoalText;
+
+        
+    private int _CurrentCharacter;
+    
+    public int ChaptersCompleted { get; private set; }
+    public int TotalChapters => _GoalText.Length; 
+    
+
+
+    public void TestInput(char input)
+    {
+        if (input == _GoalText[ChaptersCompleted][_CurrentCharacter])
+        {
+            _CurrentCharacter++;
+            if (_CurrentCharacter >= _GoalText.Length)
+            {
+                CompletedChapter();
+            }
+        }
+    }
+
+    private void CompletedChapter()
+    {
+        ChaptersCompleted++;
+        Debug.Log("Chapter Completed: Congrats");
+
+        if (ChaptersCompleted >= TotalChapters + 1)
+        {
+            Debug.Log("Book Finished: Congratulations!");
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/TextBoxLogic/TextBoxBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBoxBehaviour : MonoBehaviour
+{
+    public Texture2D textureToDisplay;
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(550, 600, 500, 500), "Hello World!");
+        GUI.Label(new Rect(100, 400, textureToDisplay.width, textureToDisplay.height), textureToDisplay);
+    }
+}
+
+
+
+
+//     private string _textName = "text";
+//
+//     public string TextName
+//     {
+//         get { return _textName; }
+//         set
+//         {
+//             _textName = value;
+//             // RaisePropertyChanged(nameof(TextName));
+//         }
+//     }
+
+
+// <StackPanel>
+// <TextBlock Text = "{Binding TextName}" FontSize = "30"></TextBlock>
+// <TextBox Text = "{Binding TextName, 
+// UpdateSourceTrigger = PropertyChanged}">
+// </TextBox>
+// </StackPanel>
