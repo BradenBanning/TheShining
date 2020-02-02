8ceab948ce7bc01b2ac8361ed6ebba2563008c1a
Braden <Braden.Banning@gmail.com>
2020-02-01 23:12:03 -0800

    Text Reader and Inputs Intergrated

--- Assets/Scripts/Scriptables/SOTextBoxBehaviour.cs
+++ Assets/Scripts/Scriptables/SOTextBoxBehaviour.cs
@@ -4,25 +4,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SOTextBoxBehaviour : MonoBehaviour
+[CreateAssetMenu(menuName = "Scriptables/TextBox")]
+public class SOTextBoxBehaviour : ScriptableObject
 {
     
     [SerializeField] private String[] _GoalText;
 
         
     private int _CurrentCharacter;
-    
-    public int ChaptersCompleted { get; private set; }
-    public int TotalChapters => _GoalText.Length; 
-    
+
+    public int ChaptersCompleted { get; private set; } = 0;
+    public int TotalChapters => _GoalText.Length;
 
 
+    public void Initialize()
+    {
+        _CurrentCharacter = 0;
+        ChaptersCompleted = 0;
+    }
+    
+        
     public void TestInput(char input)
     {
-        if (input == _GoalText[ChaptersCompleted][_CurrentCharacter])
+        if (input == _GoalText[ChaptersCompleted].ToUpper()[_CurrentCharacter])
         {
             _CurrentCharacter++;
-            if (_CurrentCharacter >= _GoalText.Length)
+            Debug.Log($"Hit! : Current Character -> {_CurrentCharacter} : GoalText Length -> {_GoalText[ChaptersCompleted].Length}");
+            if (_CurrentCharacter >= _GoalText[ChaptersCompleted].Length)
             {
                 CompletedChapter();
             }
@@ -32,9 +40,10 @@ public class SOTextBoxBehaviour : MonoBehaviour
     private void CompletedChapter()
     {
         ChaptersCompleted++;
+        _CurrentCharacter = 0;
         Debug.Log("Chapter Completed: Congrats");
 
-        if (ChaptersCompleted >= TotalChapters + 1)
+        if (ChaptersCompleted >= TotalChapters)
         {
             Debug.Log("Book Finished: Congratulations!");
         }
