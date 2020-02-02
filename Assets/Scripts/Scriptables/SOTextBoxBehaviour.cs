3b4cf2028cee27ea9ec82ce53f5c444fa031ed12
Braden <Braden.Banning@gmail.com>
2020-02-01 23:45:04 -0800

    Text Writer behavior Cleaned Up

--- Assets/Scripts/Scriptables/SOTextBoxBehaviour.cs
+++ Assets/Scripts/Scriptables/SOTextBoxBehaviour.cs
@@ -7,30 +7,31 @@ using UnityEngine.UI;
 [CreateAssetMenu(menuName = "Scriptables/TextBox")]
 public class SOTextBoxBehaviour : ScriptableObject
 {
-    
     [SerializeField] private String[] _GoalText;
+    [SerializeField] private string _WinStateMent = "You Win!!!!";
 
-        
-    private int _CurrentCharacter;
 
+    public int CurrentCharacter { get; private set; }
     public int ChaptersCompleted { get; private set; } = 0;
     public int TotalChapters => _GoalText.Length;
 
 
     public void Initialize()
     {
-        _CurrentCharacter = 0;
+        CurrentCharacter = 0;
         ChaptersCompleted = 0;
     }
-    
-        
+
+
     public void TestInput(char input)
     {
-        if (input == _GoalText[ChaptersCompleted].ToUpper()[_CurrentCharacter])
+        if (ChaptersCompleted >= TotalChapters) return;
+        if (input == _GoalText[ChaptersCompleted].ToUpper()[CurrentCharacter])
         {
-            _CurrentCharacter++;
-            Debug.Log($"Hit! : Current Character -> {_CurrentCharacter} : GoalText Length -> {_GoalText[ChaptersCompleted].Length}");
-            if (_CurrentCharacter >= _GoalText[ChaptersCompleted].Length)
+            CurrentCharacter++;
+            Debug.Log(
+                $"Hit! : Current Character -> {CurrentCharacter} : GoalText Length -> {_GoalText[ChaptersCompleted].Length}");
+            if (CurrentCharacter >= _GoalText[ChaptersCompleted].Length)
             {
                 CompletedChapter();
             }
@@ -39,20 +40,19 @@ public class SOTextBoxBehaviour : ScriptableObject
 
     private void CompletedChapter()
     {
-        ChaptersCompleted++;
-        _CurrentCharacter = 0;
+        CurrentCharacter = 0;
         Debug.Log("Chapter Completed: Congrats");
+            ChaptersCompleted++;
 
         if (ChaptersCompleted >= TotalChapters)
         {
             Debug.Log("Book Finished: Congratulations!");
         }
-        
     }
 
     public string GetGoalText()
     {
+        if (ChaptersCompleted >= TotalChapters) return _WinStateMent;
         return _GoalText[ChaptersCompleted];
     }
-
 }
--- Assets/Scripts/TextBoxLogic/TextBoxBehaviour.cs
+++ Assets/Scripts/TextBoxLogic/TextBoxBehaviour.cs
@@ -10,12 +10,28 @@ public class TextBoxBehaviour : MonoBehaviour
     [SerializeField] private SOTextBoxBehaviour _TextBoxRef;
 
     [SerializeField] private TextMeshProUGUI _CurrentChapterDisplay;
-    
+
     [SerializeField] private TextMeshProUGUI _TypingDisplay;
 
 
     private void Update()
     {
         _CurrentChapterDisplay.text = _TextBoxRef.GetGoalText();
+        if (_TextBoxRef.CurrentCharacter > 0)
+        {
+            string currentTypingText = "";
+            for (int i = 0; i < _TextBoxRef.CurrentCharacter ; i++)
+            {
+               currentTypingText = currentTypingText.Insert(currentTypingText.Length, _TextBoxRef.GetGoalText()[i].ToString());
+            }
+            Debug.Log(currentTypingText);
+
+            _TypingDisplay.text = currentTypingText;
+        }
+
+        else
+        {
+            _TypingDisplay.text = "";
+        }
     }
 }
