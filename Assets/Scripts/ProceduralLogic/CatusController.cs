da37c036698b694d404e7e6ddff717bd22636e6e
Braden <Braden.Banning@gmail.com>
2020-02-01 19:26:32 -0800

    Catus Enable Randomizer Added

--- Assets/Scripts/ProceduralLogic/CatusController.cs
+++ Assets/Scripts/ProceduralLogic/CatusController.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections;
+﻿using System;using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CatusController : MonoBehaviour
 {
@@ -10,5 +10,36 @@ public class CatusController : MonoBehaviour
     private void Awake()
     {
         _CactusArray = GetComponentsInChildren<IsCactus>();
+        for (int i = 0; i < _CactusArray.Length; i++)
+        {
+            _CactusArray[i].gameObject.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        var range = Random.Range(0, _CactusArray.Length);
+        
+        var count =  Mathf.Clamp(range / 3, 0, 2);
+
+        for (int i = 0; i < count; i++)
+        {
+            Debug.Log(range % _CactusArray.Length);
+            _CactusArray[range % _CactusArray.Length].gameObject.SetActive(true);
+            range += _CactusArray.Length / 2;
+        }
+        
+        
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < _CactusArray.Length; i++)
+        {
+            if (_CactusArray[i].gameObject.activeInHierarchy == true)
+            {
+                _CactusArray[i].gameObject.SetActive(false);
+            }
+        }
     }
 }
