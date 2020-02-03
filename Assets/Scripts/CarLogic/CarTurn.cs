9ed60b206df90d23ac4d0bbbd2c07529ee83b5b7
Braden <Braden.Banning@gmail.com>
2020-02-02 16:18:29 -0800

    GameOver Event and GameOver ColourChange Added

--- Assets/Scripts/CarLogic/CarTurn.cs
+++ Assets/Scripts/CarLogic/CarTurn.cs
@@ -48,9 +48,9 @@ public class CarTurn : MonoBehaviour
             _DashBoardRef.Ref.Value.transform.rotation.y * Mathf.Rad2Deg >= _TurnMaximum) return;
 
         _DashBoardRef.Ref.Value.transform.Rotate(_TurnDirection * _RotationAmount);
-        _RoadChunksRef.Ref.Value.transform.Rotate((-_TurnDirection * _RotationAmount));
+        _RoadChunksRef.Instance.Value.transform.Rotate((-_TurnDirection * _RotationAmount));
 
-        if (_TurnDirection == 1f || (_DashBoardRef.Ref.Value.transform.rotation.y > 0f && _RoadChunksRef.Ref.Value.transform.rotation.y > 0f)) return;
+        if (_TurnDirection == 1f || (_DashBoardRef.Ref.Value.transform.rotation.y > 0f && _RoadChunksRef.Instance.Value.transform.rotation.y > 0f)) return;
 
         ResetCarRotation();
     }
---
+++ Assets/Scripts/Scriptables/SOGameOverEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptables/GameOverEvent")]
+public class SOGameOverEvent : ScriptableObject
+{
+    public Action GameOverHappened = delegate { };
+    public Action<bool> InsideGameOverZone = delegate(bool b) {  };
+    
+}
